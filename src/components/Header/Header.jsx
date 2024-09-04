35ca89c2c4bde2be28a881ca8898afdb6b0c4ef4
Zafar <Daniya.Zafar@analog.com>
2024-09-04 13:42:21 -0700

    GL-7-Header-Component: Added responsive header for all breakpoints and added default padding for body in App.scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import logo from '../../assets/logos/InStock-Logo_1x.png';
+import { Link } from 'react-router-dom';
+
 import "./Header.scss"
 
 function Header() {
   return (
-    <div>
-
-    </div>
+    <section className='header'>
+      <img className='header-logo' src={logo} alt="In Stock Logo" />
+      <section className='header-links'>
+        <h3><Link className='header-links__item warehouse-link' to='/warehouses'>Warehouses</Link></h3>
+        <h3><Link className='header-links__item inventory-link' to='/inventory'>Inventory</Link></h3>
+      </section>
+    </section>
   )
 }
 
