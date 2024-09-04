ea525bb4f99d0bdf4d949f5d25edc0a0ce04a5b6
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-04 15:46:57 -0600

    create initial html structure for warehouses edit page

--- src/pages/Warehouses/EditWarehouse/EditWarehouse.jsx
+++ src/pages/Warehouses/EditWarehouse/EditWarehouse.jsx
@@ -1,11 +1,103 @@
 import React from 'react'
+import { ReactSVG } from "react-svg";
+import ArrowBackIcon from "../../../assets/icons/arrow_back-24px.svg"
 import './EditWarehouse.scss'
 
 function EditWarehouse() {
   return (
-    <div>
+    <div className="edit">
+      <div className="edit__header">
+        <h1 className="edit__header-title">Edit Warehouse</h1>
+        <ReactSVG className="edit__header-icon" src={ArrowBackIcon} />
+      </div>
+      <form className="edit__form">
+        <div className="edit__form-warehouse">
+          <h2 className="edit__form-warehouse-title">Warehouse Details</h2>
+          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-name">Warehouse Name</label>
+          <input
+            className="edit__form-warehouse-name"
+            id="edit__form-warehouse-name"
+            type="text"
+            placeholder="Warehouse Name"
+            name="name"
+          />
+          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-street">Street Address</label>
+          <input
+            className="edit__form-warehouse-street"
+            id="edit__form-warehouse-street"
+            type="text"
+            placeholder="Street Address"
+            name="street"
+          />
+          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-city">City</label>
+          <input
+            className="edit__form-warehouse-city"
+            id="edit__form-warehouse-city"
+            type="text"
+            placeholder="City"
+            name="city"
+          />
+          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-country">Country</label>
+          <input
+            className="edit__form-warehouse-country"
+            id="edit__form-warehouse-country"
+            type="text"
+            placeholder="Country"
+            name="country"
+          />
+        </div>
 
-    </div>
+
+
+
+
+
+
+        <div className="edit__form-contact">
+        <h2 className="edit__form-contact-title">Contact Details</h2>
+          <label className="edit__form-contact-label" htmlFor="edit__form-contact-name">Contact Name</label>
+          <input
+            className="edit__form-contact-name"
+            id="edit__form-contact-name"
+            type="text"
+            placeholder="Full Name"
+            name="name"
+          />
+
+          <label className="edit__form-contact-label" htmlFor="edit__form-contact-position">Position</label>
+          <input
+            className="edit__form-contact-position"
+            id="edit__form-contact-position"
+            type="text"
+            placeholder="Position"
+            name="position"
+          />
+
+          <label className="edit__form-contact-label" htmlFor="edit__form-contact-phone">Phone Number</label>
+          <input
+            className="edit__form-contact-phone"
+            id="edit__form-contact-phone"
+            type="text"
+            placeholder="Phone Number"
+            name="phone"
+          />
+
+          <label className="edit__form-contact-label" htmlFor="edit__form-contact-email">Email</label>
+          <input
+            className="edit__form-contact-email"
+            id="edit__form-contact-email"
+            type="text"
+            placeholder="Email"
+            name="email"
+          />                              
+        </div>
+        <div className="edit__form-buttons">
+          <div className="edit__form-buttons-save">Save</div>
+          <div className="edit__form-buttons-cancel">Cancel</div>
+        </div>
+
+      </form>
+    </div >
   )
 }
 
