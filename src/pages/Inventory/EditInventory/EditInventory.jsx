eaa379fa80fdf291b2e723e26bb2b5e1eb041d2f
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-05 11:49:11 -0700

    header title added

--- src/pages/Inventory/EditInventory/EditInventory.jsx
+++ src/pages/Inventory/EditInventory/EditInventory.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { ReactSVG } from "react-svg";
+import ArrowBackIcon from "../../../assets/icons/arrow_back-24px.svg";
+import SortIcon from "../../../assets/icons/sort-24px.svg";
 import './EditInventory.scss'
 
 function EditInventory() {
   return (
-    <div>
-
+    <div className="inventory">
+      <header className="inventory__title">
+        <ReactSVG src={ArrowBackIcon} />
+        <h1 className="inventory__title-heading">Edit Inventory Item</h1>
+      </header>
+      <h3>
+        WAREHOUSE
+        <ReactSVG src={SortIcon} />
+      </h3>
     </div>
   )
 }
