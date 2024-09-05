0bf41daf50977d8a3d5debee1462d5ac2a25e7a1
Darvy Yu <thedarvy@gmail.com>
2024-09-05 11:53:46 -0700

    delete debug console

--- src/pages/Warehouses/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/Warehouses/WarehouseDetails/WarehouseDetails.jsx
@@ -5,10 +5,8 @@ import Inventory from '../../Inventory/Inventory.jsx';
 
 function WarehouseDetails() {
   const params = useParams();
-  console.log(params.warehouseId, params)
   return (
     <div>
-      <div>test</div>
       <Inventory warehouseId={params.warehouseId}/>
     </div>
   )
