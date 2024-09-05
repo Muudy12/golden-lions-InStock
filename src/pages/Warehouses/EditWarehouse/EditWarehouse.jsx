dfae5f8382ad76e379cd4679d2e503c6563d5c1d
Bahar Hamzeh <blikebahar76@gmail.com>
2024-09-05 00:43:44 -0600

    Add styling to inputs and labels and buttons of  warehouse edit page

--- src/pages/Warehouses/EditWarehouse/EditWarehouse.jsx
+++ src/pages/Warehouses/EditWarehouse/EditWarehouse.jsx
@@ -13,33 +13,33 @@ function EditWarehouse() {
       <form className="edit__form">
         <div className="edit__form-warehouse">
           <h2 className="edit__form-warehouse-title">Warehouse Details</h2>
-          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-name">Warehouse Name</label>
+          <label className="edit__form-label" htmlFor="edit__form-warehouse-name">Warehouse Name</label>
           <input
-            className="edit__form-warehouse-name"
+            className="edit__form-input"
             id="edit__form-warehouse-name"
             type="text"
             placeholder="Warehouse Name"
             name="name"
           />
-          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-street">Street Address</label>
+          <label className="edit__form-label" htmlFor="edit__form-warehouse-street">Street Address</label>
           <input
-            className="edit__form-warehouse-street"
+            className="edit__form-input"
             id="edit__form-warehouse-street"
             type="text"
             placeholder="Street Address"
             name="street"
           />
-          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-city">City</label>
+          <label className="edit__form-label" htmlFor="edit__form-warehouse-city">City</label>
           <input
-            className="edit__form-warehouse-city"
+            className="edit__form-input"
             id="edit__form-warehouse-city"
             type="text"
             placeholder="City"
             name="city"
           />
-          <label className="edit__form-warehouse-label" htmlFor="edit__form-warehouse-country">Country</label>
+          <label className="edit__form-label" htmlFor="edit__form-warehouse-country">Country</label>
           <input
-            className="edit__form-warehouse-country"
+            className="edit__form-input"
             id="edit__form-warehouse-country"
             type="text"
             placeholder="Country"
@@ -48,33 +48,33 @@ function EditWarehouse() {
         </div>
         <div className="edit__form-contact">
         <h2 className="edit__form-contact-title">Contact Details</h2>
-          <label className="edit__form-contact-label" htmlFor="edit__form-contact-name">Contact Name</label>
+          <label className="edit__form-label" htmlFor="edit__form-contact-name">Contact Name</label>
           <input
-            className="edit__form-contact-name"
+            className="edit__form-input"
             id="edit__form-contact-name"
             type="text"
             placeholder="Full Name"
             name="name"
           />
-          <label className="edit__form-contact-label" htmlFor="edit__form-contact-position">Position</label>
+          <label className="edit__form-label" htmlFor="edit__form-contact-position">Position</label>
           <input
-            className="edit__form-contact-position"
+            className="edit__form-input"
             id="edit__form-contact-position"
             type="text"
             placeholder="Position"
             name="position"
           />
-          <label className="edit__form-contact-label" htmlFor="edit__form-contact-phone">Phone Number</label>
+          <label className="edit__form-label" htmlFor="edit__form-contact-phone">Phone Number</label>
           <input
-            className="edit__form-contact-phone"
+            className="edit__form-input"
             id="edit__form-contact-phone"
             type="text"
             placeholder="Phone Number"
             name="phone"
           />
-          <label className="edit__form-contact-label" htmlFor="edit__form-contact-email">Email</label>
+          <label className="edit__form-label" htmlFor="edit__form-contact-email">Email</label>
           <input
-            className="edit__form-contact-email"
+            className="edit__form-input"
             id="edit__form-contact-email"
             type="text"
             placeholder="Email"
@@ -82,8 +82,8 @@ function EditWarehouse() {
           />                              
         </div>
         <div className="edit__form-buttons">
-          <div className="edit__form-buttons-save">Save</div>
           <div className="edit__form-buttons-cancel">Cancel</div>
+          <div className="edit__form-buttons-save">Save</div>
         </div>
       </form>
     </div >
