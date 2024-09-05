7a7d822323b88ed9054f3b7132cf8f55722f14f7
Darvy Yu <thedarvy@gmail.com>
2024-09-05 11:52:15 -0700

    adding iventories given warehouse id, adding untils'

--- src/pages/Warehouses/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/Warehouses/WarehouseDetails/WarehouseDetails.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './WarehouseDetails.scss'
+import { useParams } from "react-router-dom";
+import Inventory from '../../Inventory/Inventory.jsx';
 
 function WarehouseDetails() {
+  const params = useParams();
+  console.log(params.warehouseId, params)
   return (
     <div>
-
+      <div>test</div>
+      <Inventory warehouseId={params.warehouseId}/>
     </div>
   )
 }
