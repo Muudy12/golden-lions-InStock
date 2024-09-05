3d233e0a47281ca2a3d9bea83b6806dd4d441e27
Zafar <Daniya.Zafar@analog.com>
2024-09-05 11:46:44 -0700

    updated inventory route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
       <Link className='header-link' to="/"><img className='header-logo' src={logo} alt="In Stock Logo" /></Link>
       <section className='header-links'>
         <h3><Link className={`header-links__item ${location.pathname.includes('warehouses')||location.pathname === '/'?'active-link':''}`} to='/warehouses'>Warehouses</Link></h3>
-        <h3><Link className={`header-links__item ${location.pathname.includes('inventory')?'active-link':''}`} to='/inventories'>Inventory</Link></h3>
+        <h3><Link className={`header-links__item ${location.pathname.includes('inventory')?'active-link':''}`} to='/inventory'>Inventory</Link></h3>
       </section>
     </section>
   )
