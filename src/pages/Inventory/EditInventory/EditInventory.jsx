7329ede7fb236d4a3601351a6d4b877d08001d0a
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-05 20:56:39 -0700

    labels added for form

--- src/pages/Inventory/EditInventory/EditInventory.jsx
+++ src/pages/Inventory/EditInventory/EditInventory.jsx
@@ -5,15 +5,33 @@ import './EditInventory.scss'
 
 function EditInventory() {
   return (
-    <div className="inventory">
-      <header className="inventory__title">
-        <ReactSVG src={ArrowBackIcon} />
-        <h1 className="inventory__title-heading">Edit Inventory Item</h1>
+    <div className="edit-inventory">
+      <header className="edit-inventory__title">
+        <img src={ArrowBackIcon} />
+        <h1 className="edit-inventory__title-heading">Edit Inventory Item</h1>
       </header>
-      <h3>
-        WAREHOUSE
-        <ReactSVG src={SortIcon} />
-      </h3>
+      <form className="form">
+        <div className="form__container">
+          <h2 className="form__title">Item Details</h2>
+          <h3 className="form__label">Item Name</h3>
+          <input className="form__input" type="text" placeholder="Television" name="item_name" />
+
+          <h3 className="form__label">Description</h3>
+          <input className="form__input" type="text" placeholder="Television" name="item_name" />
+
+          <h3 className="form__label">Category</h3>
+
+          <h2 className="form__title">Item Availability</h2>
+          <h3 className="form__label">Status</h3>
+
+          <h3 className="form__label">Quantity</h3>
+
+          <h3 className="form__label">Warehouse</h3>
+          <select className="form__input" type="text" placeholder="Television" name="item_name">
+            <option>Washington</option>
+          </select>
+        </div>
+      </form>
     </div>
   )
 }
