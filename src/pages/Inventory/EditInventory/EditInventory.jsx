35d62175b8ae9921808b6f392eb8c6eace83eff0
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-05 22:07:36 -0700

    styling added for for inputs

--- src/pages/Inventory/EditInventory/EditInventory.jsx
+++ src/pages/Inventory/EditInventory/EditInventory.jsx
@@ -12,26 +12,44 @@ function EditInventory() {
       </header>
       <form className="form">
         <div className="form__container">
-          <h2 className="form__title">Item Details</h2>
-          <h3 className="form__label">Item Name</h3>
-          <input className="form__input" type="text" placeholder="Television" name="item_name" />
+          <section className="form__section">
+            <h2 className="form__title">Item Details</h2>
+            <h3 className="form__label">Item Name</h3>
+            <input className="form__input" type="text" placeholder="Television" name="item_name" />
 
-          <h3 className="form__label">Description</h3>
-          <input className="form__input" type="text" placeholder="Television" name="item_name" />
+            <h3 className="form__label">Description</h3>
+            <textarea className="form__textarea" placeholder='This 50", 4K LED TV provides a crystal-clear picture and vivid colors.' name="item_name" />
 
-          <h3 className="form__label">Category</h3>
+            <h3 className="form__label">Category</h3>
+            <select className="form__select" type="text" placeholder="Television" name="item_name" >
+              <option value='Electronics'>Electronics</option>
+            </select>
+          </section>
 
-          <h2 className="form__title">Item Availability</h2>
-          <h3 className="form__label">Status</h3>
+          <aside className="form__aside">
+            <h2 className="form__title">Item Availability</h2>
 
-          <h3 className="form__label">Quantity</h3>
+            <h3 className="form__label">Status</h3>
+            <input className="form__radio-btn" type="radio" name="status" value='In Stock' />
+            <label for='In Stock'>In Stock</label>
+            <input className="form__radio-btn" type="radio" name="status" value='Out of Stock' />
+            <label for='In Stock'>Out of Stock</label>
 
-          <h3 className="form__label">Warehouse</h3>
-          <select className="form__input" type="text" placeholder="Television" name="item_name">
-            <option>Washington</option>
-          </select>
+            <h3 className="form__label">Quantity</h3>
+            <input className="form__input" type="text" placeholder="500" name="item_name" />
+
+            <h3 className="form__label">Warehouse</h3>
+            <select className="form__select" type="text" name="item_name">
+              <option>Washington</option>
+            </select>
+          </aside>
+
         </div>
       </form>
+      <div className="edit-inventory__bottom">
+        <button className="edit-inventory__cancel-btn">Cancel</button>
+        <button className="edit-inventory__cancel-btn">Save</button>
+      </div>
     </div>
   )
 }
