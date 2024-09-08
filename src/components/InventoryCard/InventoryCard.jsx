3d2fdf497a02ce32f30d55858d945e4dc30546f2
Darvy Yu <thedarvy@gmail.com>
2024-09-08 10:14:46 -0700

    fix add iventory page  warning and empty wanrning requird message

--- src/components/InventoryCard/InventoryCard.jsx
+++ src/components/InventoryCard/InventoryCard.jsx
@@ -5,23 +5,33 @@ import { Api } from "../../utils/utils.js"
 
 import './InventoryCard.scss'
 import ArrowBackIcon from "../../assets/icons/arrow_back-24px.svg"
+import FormError from '../FormError/FormError.jsx';
 
 function InventoryCard({cardTitle, inventoryItem}) {
 
   const api  = new Api();
   const navigate = useNavigate();
-  const [inStock, setInStock] = useState(false);
+  const [inStock, setInStock] = useState(true);
   const [createInventoryPage, setCreateInventoryPage] = useState(true);
   const [inventoryList, setinventoryList] = useState([])
   const [inventoryItemData, setInventoryItemData] = useState({
     item_name: inventoryItem?.item_name || '',
     description: inventoryItem?.description || '',
     category: inventoryItem?.category || '',
-    status: inventoryItem?.status || 'Out of Stock',
-    quantity: inventoryItem?.quantity || null,
+    status: inventoryItem?.status || 'In Stock',
+    quantity: inventoryItem?.quantity || 1,
     warehouse_id: inventoryItem?.warehouse_id || null});
   const warehouseNameRegex = /"(.*?)"/;
   const warehouseIdRegex = /,(.*?)\]/;
+
+  const [formErrors, setFormErrors] = useState({
+    item_name: { isValid: true, errorMessage: '' },
+    description: { isValid: true, errorMessage: '' },
+    category: { isValid: true, errorMessage: '' },
+    status: { isValid: true, errorMessage: '' },
+    quantity: { isValid: true, errorMessage: '' },
+    warehouse_id: { isValid: true, errorMessage: '' }
+  })
 
   if (inventoryItem){
     setCreateInventoryPage(false);
@@ -41,6 +51,9 @@ function InventoryCard({cardTitle, inventoryItem}) {
 
   const handleChange = (event) => {
     const { name, value, type, checked } = event.target;
+
+    validationCheck(name, value);
+
     setInventoryItemData((prevData) => ({
         ...prevData,
           [name]: name ==='quantity'|| name ==='warehouse_id' ? parseInt(value, 10):value
@@ -52,6 +65,18 @@ function InventoryCard({cardTitle, inventoryItem}) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    let formIsValid = true;
+    Object.entries(inventoryItemData).forEach(([name, value]) => {
+      if (!validationCheck(name, value)) {
+        formIsValid = false;
+      }
+    })
+
+    if (!formIsValid) {
+      return
+    }
+
     if(inventoryItemData.status === "Out of Stock"){
       inventoryItemData.quantity = 0;
     }else if (inventoryItemData.quantity === 0){
@@ -64,6 +89,28 @@ function InventoryCard({cardTitle, inventoryItem}) {
 
   const handlePageChange = () => {
     navigate(`/inventory`);
+  }
+
+  const validationCheck = (name, value) => {
+    let isValid = true;
+    let errorMessage = '';
+
+    if (typeof value === 'string' && value.trim() === '') {
+      isValid = false;
+      errorMessage = `${name.replace('_', ' ')} cannot be empty`
+    }
+
+    if (name === 'quantity' && inStock && isNaN(value)) {
+      isValid = false;
+      errorMessage = 'Quantity must be a number';
+    }
+
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: { isValid, errorMessage },
+    }))
+
+    return isValid;
   }
 
 
@@ -85,8 +132,9 @@ function InventoryCard({cardTitle, inventoryItem}) {
               name="item_name"
               value={inventoryItemData.item_name}
               onChange={handleChange}
-              required
             />
+            <FormError isValid={formErrors.item_name.isValid} errorMessage={formErrors.item_name.errorMessage} />
+
             <h3 className="inventory-card__form-label">Description</h3>
             <textarea
               className="inventory-card__form-input"
@@ -95,12 +143,13 @@ function InventoryCard({cardTitle, inventoryItem}) {
               name="description"
               value={inventoryItemData.description}
               onChange={handleChange}
-              required
             />
+            <FormError isValid={formErrors.description.isValid} errorMessage={formErrors.description.errorMessage} />
+
             <h3 className="inventory-card__form-label">Category</h3>
             <select className="inventory-card__form-dropdown" 
             name="category" 
-            value={inventoryItemData.category}
+            value={inventoryItemData.category||""}
             onChange={handleChange}>
               <option value="Please select">Please select</option>
               {categoryTypes?.map((type,idx)=>(
@@ -142,16 +191,17 @@ function InventoryCard({cardTitle, inventoryItem}) {
               className="inventory-card__form-input"
               type="number"
               placeholder="1"
-              min="0"
+              min="1"
               name="quantity"
-              value={inventoryItemData.quantity}
+              value={inventoryItemData.quantity||0}
               onChange={handleChange}
-              required
             /></span>}
+            <FormError isValid={formErrors.quantity.isValid} errorMessage={formErrors.quantity.errorMessage} />
+
             <h3 className="inventory-card__form-label">Warehouse</h3>
             <select className='inventory-card__form-dropdown' 
             name="warehouse_id"
-            value={inventoryItemData.warehouse_id}
+            value={inventoryItemData.warehouse_id||""}
             onChange={handleChange}>
               <option value="Please select">Please select</option>
               {warehouseTypes?.map((type,idx)=>(
